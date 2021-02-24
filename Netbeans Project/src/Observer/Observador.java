02995e7f6d2bfd3b24cc236245fabba6fdf17533
VictorLavalle <45529198+VictorLavalle@users.noreply.github.com>
2021-02-23 20:13:17 -0600

    Deleted Unused Files

--- Netbeans Project/src/Observer/Observador.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-/**
- * @author Gabriel Islas
- * @author Victor Lavalle
- * @auhtor Eberth Mezeta
- */
-public interface Observador {
-    public void update(String nuevoProducto);
-}
